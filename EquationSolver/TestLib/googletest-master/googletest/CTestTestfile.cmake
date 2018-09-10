ed3bb0bc5b2c64a18600104249304f1ede6c5aa7
gazizullin <gazizullin.malik2010@yandex.ru>
2018-09-10 22:47:01 +0300

    Not finished yet, only quadric

---
+++ EquationSolver/TestLib/googletest-master/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/Studying/MIPT/term 5/extra acos/hw/EquationSolver/TestLib/googletest-master
+# Build directory: /home/user/Desktop/Studying/MIPT/term 5/extra acos/hw/EquationSolver/TestLib/googletest-master/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")

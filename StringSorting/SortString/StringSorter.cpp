7fac2225898dd276539c6d4fbe6f26cb3d2a0ac9
gazizullin <gazizullin.malik2010@yandex.ru>
2018-09-16 21:37:52 +0300

    A little bit better now

--- StringSorting/SortString/StringSorter.cpp
+++ StringSorting/SortString/StringSorter.cpp
@@ -12,7 +12,42 @@
 #include "../ReadFullFile/FullFileReader.h"
 
 namespace StringSorter {
-    bool isSpecial(char i);
+    bool isSpecial(char i) {
+        return !((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'));
+    }
+    bool comparator(const char* a,
+                    const char* b,
+                    size_t firstStart,
+                    size_t secondStart,
+                    int firstIncrement,
+                    int secondIncrement,
+                    size_t firstEnd,
+                    size_t secondEnd) {
+        // empty -> to the end
+        if (firstEnd == firstStart) {
+            return false;
+        }
+        if (secondEnd == secondStart) {
+            return true;
+        }
+        while (firstEnd != firstStart && secondEnd != secondStart) {
+            if (isSpecial(a[firstStart]) || isSpecial(b[secondStart])) {
+                if (isSpecial(a[firstStart])) {
+                    firstStart += firstIncrement;
+                }
+                if (isSpecial(b[secondStart])) {
+                    secondStart += secondIncrement;
+                }
+            } else if (a[firstStart] != b[secondStart]) {
+                return a[firstStart] < b[secondStart];
+            } else {
+                firstStart += firstIncrement;
+                secondStart += secondIncrement;
+            }
+        }
+        // if smaller -> less
+        return firstEnd == firstStart;
+    }
 
     void sortMyFile(char *SrcFileName, char *DistFileName) {
         char* text;
@@ -20,74 +55,26 @@ namespace StringSorter {
         size_t * indexes;
         size_t countOfLines = FullFileReader::changeSlashesToNulles(text, &indexes);
 
-        std::sort(indexes, indexes + countOfLines, [indexes, &text] (size_t a, size_t b) {
-            if (text[a] == '\0' || text[b] == '\0') {
-                return text[a] != '\0'; // Empty lines at the end)
-            }
-            size_t i = strlen(text + a) - 1;
-            size_t j = strlen(text + b) - 1;
-            while (i >= 0 && j >= 0) {
-                if (isSpecial(text[a + i]) || isSpecial(text[b + j])) {
-                    if (isSpecial(text[a + i])) {
-                        if (i == 0) {
-                            return true;
-                        }
-                        i--;
-                    }
-                    if (isSpecial(text[b + j])) {
-                        if (j == 0) {
-                            return false;
-                        }
-                        j--;
-                    }
-                } else if (text[a + i] != text[b + j]) {
-                    return text[a + i] < text[b + j];
-                } else {
-                    if (i == 0 || j == 0) {
-                        if (i == 0) {
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }
-                    i--, j--;
-                }
-            }
-            return true;
+        size_t *standartIndexes = new size_t[countOfLines + 1];
+        std::copy(indexes, indexes + countOfLines, standartIndexes);
+
+        std::sort(indexes, indexes + countOfLines, [&text] (size_t a, size_t b) {
+            return comparator(text + a, text + b, 0, 0, 1, 1, (strlen(text + a) == 0) ? 0 : strlen(text + a) - 1,
+                              (strlen(text + b) == 0) ? 0 : strlen(text + b) - 1);
         });
 
-        FullFileReader::outputInFile(indexes, text, DistFileName, countOfLines, O_CREAT | O_WRONLY);
-        std::sort(indexes, indexes + countOfLines, [indexes, &text] (size_t a, size_t b) {
-            if (text[a] == '\0' || text[b] == '\0') {
-                return text[a] != '\0'; // Empty lines at the end)
-            }
-            for (size_t i = 0, j = 0; i < strlen(reinterpret_cast<const char *>(text + a)) &&
-                    j < strlen(reinterpret_cast<const char *>(text + b));) {
-                if (text[a + i] != text[b + j]) {
-                    return text[a + i] < text[b + j];
-                } else if (isSpecial(text[a + i]) || isSpecial(text[b + j])) {
-                    if (isSpecial(text[a + i])) {
-                        i++;
-                    }
-                    if (isSpecial(text[b + j])) {
-                        j++;
-                    }
-                } else {
-                    i++, j++;
-                }
-            }
-            return true;
+        FullFileReader::outputInFile(indexes, text, DistFileName, countOfLines, O_CREAT | O_WRONLY | O_TRUNC);
+
+
+        std::sort(indexes, indexes + countOfLines, [&text] (size_t a, size_t b) {
+            return comparator(text + a, text + b, (strlen(text + a) == 0) ? 0 : strlen(text + a) - 1,
+                              (strlen(text + b) == 0) ? 0 : strlen(text + b) - 1, -1, -1, 0, 0);
         });
-
         FullFileReader::outputInFile(indexes, text, DistFileName, countOfLines, O_WRONLY | O_APPEND);
 
-        std::sort(indexes, indexes + countOfLines);
-        FullFileReader::outputInFile(indexes, text, DistFileName, countOfLines, O_WRONLY | O_APPEND);
+
+        FullFileReader::outputInFile(standartIndexes, text, DistFileName, countOfLines, O_WRONLY | O_APPEND);
 
     }
 
-    bool isSpecial(char i) {
-        return !((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'));
-    }
-    
 }

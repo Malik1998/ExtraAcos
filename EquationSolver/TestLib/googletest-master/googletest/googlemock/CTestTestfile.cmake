817302d50ea47c58d5624b11a3b8edf1a9ca7213
gazizullin <gazizullin.malik2010@yandex.ru>
2018-09-18 09:16:52 +0300

    new folder

--- EquationSolver/TestLib/googletest-master/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/Desktop/Studying/MIPT/term 5/extra acos/hw/EquationSolver/TestLib/googletest-master/googlemock
-# Build directory: /home/user/Desktop/Studying/MIPT/term 5/extra acos/hw/EquationSolver/TestLib/googletest-master/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- EquationSolver/TestLib/googletest-master/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/Desktop/Studying/MIPT/term 5/extra acos/hw/EquationSolver/TestLib/googletest-master/googletest
-# Build directory: /home/user/Desktop/Studying/MIPT/term 5/extra acos/hw/EquationSolver/TestLib/googletest-master/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

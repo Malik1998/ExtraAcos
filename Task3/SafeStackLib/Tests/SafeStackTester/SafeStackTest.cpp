6d8c57d35eba2438c372cf36964e5d618fed76e8
gazizullin <gazizullin.malik2010@yandex.ru>
2018-10-16 00:38:25 +0300

    Minor changes

--- Task3/SafeStackLib/Tests/SafeStackTester/SafeStackTest.cpp
+++ Task3/SafeStackLib/Tests/SafeStackTester/SafeStackTest.cpp
@@ -8,23 +8,23 @@
 
 
 TEST(SafeStackTest, FULL_NESS) {
-    auto Stack = SafeStack<size_t >(-1);
-    for (size_t i = 0; i < 10000000; i++) {
+    auto Stack = SafeStack<int>(-1);
+    for (auto i = 0; i < 10000000; i++) {
         auto error_code = Stack.push(i);
-        if (error_code != SafeStack<size_t >::OK) {
-            ASSERT_EQ(SafeStack<size_t >::ErrorPush, error_code);
+        if (error_code != SafeStack<int >::OK) {
+            ASSERT_EQ(SafeStack<int >::ErrorPush, error_code);
             break;
         }
     }
 }
 
 TEST(SafeStackTest, CHANGE_FIRST_BYTES) {
-    auto Stack = SafeStack<size_t >(-1);
+    auto Stack = SafeStack<int>(-1);
     ((int *)(&Stack))[0] = 0;
-    for (size_t i = 0; i < 10000000; i++) {
+    for (auto i = 0; i < 10000000; i++) {
         auto error_code = Stack.push(i);
-        if (error_code != SafeStack<size_t >::OK) {
-            ASSERT_EQ(SafeStack<size_t >::ErroKanareika, error_code);
+        if (error_code != SafeStack<int >::OK) {
+            ASSERT_EQ(SafeStack<int >::ErroKanareika, error_code);
             break;
         }
     }
@@ -33,12 +33,12 @@ TEST(SafeStackTest, CHANGE_FIRST_BYTES) {
 // Valentin Define private: public. Портить массив, setcheckSum(); Или reintrepret_cast
 
 TEST(SafeStackTest, CHANGE_FIRST_CHECKSUM_VALENTIN) {
-    auto Stack = SafeStack<size_t >(-1);
+    auto Stack = SafeStack<int >(-1);
     ((int *)(&Stack))[107] = 0;
-    for (size_t i = 0; i < 10000000; i++) {
+    for (int i = 0; i < 10000000; i++) {
         auto error_code = Stack.push(i);
-        if (error_code != SafeStack<size_t >::OK) {
-            ASSERT_EQ(SafeStack<size_t >::ErrorPoison, error_code);
+        if (error_code != SafeStack<int >::OK) {
+            ASSERT_EQ(SafeStack<int >::ErrorPush, error_code);
             break;
         }
     }

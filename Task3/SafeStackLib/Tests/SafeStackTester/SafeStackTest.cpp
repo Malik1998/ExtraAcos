01d6de5892f973554563ce74d98553871b5932cc
gazizullin <gazizullin.malik2010@yandex.ru>
2018-10-16 00:34:42 +0300

    header changed

--- Task3/SafeStackLib/Tests/SafeStackTester/SafeStackTest.cpp
+++ Task3/SafeStackLib/Tests/SafeStackTester/SafeStackTest.cpp
@@ -5,7 +5,6 @@
 #include "SafeStackTest.h"
 #include "gtest/gtest.h"
 #include "../../SafeStack/SafeStack.h"
-#include "../../SafeStack/SafeStack.cpp"
 
 
 TEST(SafeStackTest, FULL_NESS) {
@@ -26,6 +25,20 @@ TEST(SafeStackTest, CHANGE_FIRST_BYTES) {
         auto error_code = Stack.push(i);
         if (error_code != SafeStack<size_t >::OK) {
             ASSERT_EQ(SafeStack<size_t >::ErroKanareika, error_code);
+            break;
+        }
+    }
+}
+
+// Valentin Define private: public. Портить массив, setcheckSum(); Или reintrepret_cast
+
+TEST(SafeStackTest, CHANGE_FIRST_CHECKSUM_VALENTIN) {
+    auto Stack = SafeStack<size_t >(-1);
+    ((int *)(&Stack))[107] = 0;
+    for (size_t i = 0; i < 10000000; i++) {
+        auto error_code = Stack.push(i);
+        if (error_code != SafeStack<size_t >::OK) {
+            ASSERT_EQ(SafeStack<size_t >::ErrorPoison, error_code);
             break;
         }
     }
